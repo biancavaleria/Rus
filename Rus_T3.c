268e8b4948ea93b505b23d8292f334d792af51c9
biancavaleria <61945689+biancavaleria@users.noreply.github.com>
2020-03-21 23:41:43 +0200

    Add files via upload

---
+++ Rus_T3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	int p=1, a, b=0, s, c=0, x=0;
+
+	printf("a="); scanf("%d", &a);
+	b = a;
+	while (a)
+	{
+		c = c + b;
+		a--;
+	}
+
+	while (c)
+	{
+		x = x + p * (c % 2);
+		c = c / 2;
+		p = p * 10;
+	}
+	printf("%d", x);
+
+
+	return 0;
+
+}
